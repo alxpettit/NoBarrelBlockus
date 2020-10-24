459c9dec776f178a302c9738f4adb185b1acb7d2
Alexandria <alexandria@achu.xyz>
2020-10-24 02:33:32 -0700

    weebcraft version

--- remove_nocompatible.sh
+++ remove_nocompatible.sh
@@ -2,3 +2,5 @@
 
 find src/ -iname '*barrel*' -delete
 sed -i 's/new Barrels();//' src/main/java/com/brand/blockus/Blockus.java
+sed -i 's/new Barrels();//' src/main/java/com/brand/blockus/Blockus.java
+find src/ -iname '*ender_block*' -delete
